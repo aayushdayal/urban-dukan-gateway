aa6d81b2fc2b848ae5e039c7e6b1be19b2bd5cc1
aayushdayal <dayalaayush@gmail.com>
2026-03-28 19:15:14 +0530

    Configured for production

--- Program.cs
+++ Program.cs
@@ -4,14 +4,16 @@ using Ocelot.Middleware;
 var builder = WebApplication.CreateBuilder(args);
 
 // Load Ocelot config
-builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Configuration
+    .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
+    .AddJsonFile($"ocelot.{builder.Environment.EnvironmentName}.json", optional: true);
 
 // Optional: logging
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
 
 // Optional: force port
-builder.WebHost.UseUrls("https://localhost:5000");
+// builder.WebHost.UseUrls("https://localhost:5000");
 
 // Register Ocelot
 builder.Services.AddOcelot(builder.Configuration);
